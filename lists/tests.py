c6641b371d8f96b6e08be74377c29aeefb58fbb3
Yamila Moreno <yamila.ms@gmail.com>
2015-06-04 12:01:10 +0200

    Tests passing

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,15 @@
+from django.http import HttpRequest
 from django.test import TestCase
 
+from lists.views import home_page
+
 # Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_home_page(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn('<title>To-Do Lists</title>', response.content.decode())
+        self.assertTrue(response.content.endswith(b'</html>'))
