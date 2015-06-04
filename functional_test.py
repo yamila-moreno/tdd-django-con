23517bbb7860e54cf73b195b2eaf0bb5ba75f37e
Yamila Moreno <yamila.ms@gmail.com>
2015-06-04 12:06:54 +0200

    Add views

--- functional_test.py
+++ functional_test.py
@@ -5,15 +5,15 @@ from selenium import webdriver
 class SimpleListTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()        
+        self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
-        
-    def test_starting_a_todo_list(self):        
+
+    def test_starting_a_todo_list(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test!')
-        
+
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
