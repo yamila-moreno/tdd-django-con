c7a9085ad282e6f0cd9b0c1c03a8db5af22ac07e
diacritica <diacritica@gmail.com>
2015-06-04 11:34:37 +0200

    New functional test against title

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,19 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+class SimpleListTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()        
+
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_starting_a_todo_list(self):        
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+
+        
+if __name__ == '__main__':
+    unittest.main()
